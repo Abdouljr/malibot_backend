f6ccc9f778ab38098facbdf26b949d7b3128e86b
Abdouljr <maigaabdouljr@gmail.com>
2024-11-04 11:22:54 +0000

    service et controller du groupe ok

--- controllers/groupe_controller.py
+++ controllers/groupe_controller.py
@@ -4,17 +4,19 @@ from sqlalchemy.orm import Session
 from starlette import status
 from configurations.database_config import db_dependency
 from models.groupe import GroupeDTO, GroupeBase
+from models.user import UserDTO
 from services import groupe_service, auth_service
 
 
 router = APIRouter(prefix='/groupe', tags=['Groupe'])
 user_dependency = Annotated[dict, Depends(auth_service.get_current_user)]
+message_erreur = "non authentifié"
 
 
 @router.post("/add", response_model=GroupeDTO, status_code=status.HTTP_201_CREATED)
 async def add(groupe: GroupeBase, user: user_dependency, db: Session = db_dependency):
     if user is None:
-        raise HTTPException(status_code=status.HTTP_401_UNAUTHORIZED, detail="Non authentifié")
+        raise HTTPException(status_code=status.HTTP_401_UNAUTHORIZED, detail=message_erreur)
     print(user)
     return groupe_service.add(user['uid'], groupe, db)
 
@@ -22,12 +24,26 @@ async def add(groupe: GroupeBase, user: user_dependency, db: Session = db_depend
 @router.get("/all", response_model=List[GroupeDTO], status_code=status.HTTP_200_OK)
 async def get_all(user: user_dependency, db: Session = db_dependency):
     if user is None:
-        raise HTTPException(status_code=status.HTTP_401_UNAUTHORIZED, detail="Non authentifié")
+        raise HTTPException(status_code=status.HTTP_401_UNAUTHORIZED, detail=message_erreur)
     return groupe_service.get_all(db)
+
+
+@router.get("/get-for-user", response_model=List[GroupeDTO], status_code=status.HTTP_200_OK)
+async def get_for_user(user: user_dependency, db: Session = db_dependency):
+    if user is None:
+        raise HTTPException(status_code=status.HTTP_401_UNAUTHORIZED, detail=message_erreur)
+    return groupe_service.get_groupes_for_user(db, user['uid'])
+
+
+@router.get("/add-user-to-groupe", response_model=List[UserDTO], status_code=status.HTTP_200_OK)
+async def add_user_to_groupe(groupe_id: int, user_email: str, user: user_dependency, db: Session = db_dependency):
+    if user is None:
+        raise HTTPException(status_code=status.HTTP_401_UNAUTHORIZED, detail=message_erreur)
+    return groupe_service.add_users_to_groupe(db, groupe_id, user_email)
 
 
 @router.delete("/delete/{groupe_id}", status_code=status.HTTP_200_OK)
 async def delete(user: user_dependency, groupe_id: int, db: Session = db_dependency):
     if user is None:
-        raise HTTPException(status_code=status.HTTP_401_UNAUTHORIZED, detail="Non authentifié")
+        raise HTTPException(status_code=status.HTTP_401_UNAUTHORIZED, detail=message_erreur)
     return groupe_service.supprimer_groupe(groupe_id, db)
--- services/groupe_service.py
+++ services/groupe_service.py
@@ -20,6 +20,29 @@ def get_all(db):
     return groupes
 
 
+def add_users_to_groupe(db, groupe_id: int, user_email: str):
+    # Récupère le groupe par son ID
+    groupe = db.query(Groupe).filter(Groupe.id == groupe_id).first()
+
+    if not groupe:
+        raise HTTPException(status_code=404, detail='Groupe not found')
+
+    # Récupère les utilisateurs à ajouter
+    db_user = db.query(User).filter(User.email == user_email).first()
+    if not db_user:
+        raise HTTPException(status_code=404, detail='User not found')
+
+    groupe.users.append(db_user)
+    db.commit()  # Sauvegarde les changements
+    db.refresh(groupe)  # Rafraîchit le groupe pour refléter les modifications
+    return groupe.users
+
+
+def get_groupes_for_user(db, user_unique_id: str):
+    user = db.query(User).filter(User.unique_id == user_unique_id).first()
+    return user.groupes
+
+
 def supprimer_groupe(groupe_id, db):
     db_groupe = db.query(Groupe).filter(Groupe.id == groupe_id).first()
     if db_groupe is None:
